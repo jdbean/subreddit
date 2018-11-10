07d9799e56631bb9b87e81319b678efda4ac0487
J.D. Bean <jonathandbean@gmail.com>
2018-11-10 15:01:50 -0500

    ensure that window scroll to top on route change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/Scroll-To-Top';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './index.css';
 import 'normalize.css';
-
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>
   , document.getElementById('root')
 );
