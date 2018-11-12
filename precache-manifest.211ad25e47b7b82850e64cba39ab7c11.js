d242f4b76c48feb943abf5083c171e296bb428c8
J.D. Bean <jonathandbean@gmail.com>
2018-11-12 11:20:17 -0500

    Updates

--- precache-manifest.211ad25e47b7b82850e64cba39ab7c11.js
+++ precache-manifest.211ad25e47b7b82850e64cba39ab7c11.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/subreddit/static/js/runtime~main.1293de03.js"
   },
   {
-    "revision": "611a8ab23911ae8cc8a0",
-    "url": "/subreddit/static/js/main.611a8ab2.chunk.js"
+    "revision": "f06ed6f5a8446e2453ec",
+    "url": "/subreddit/static/js/main.f06ed6f5.chunk.js"
   },
   {
     "revision": "84cf428fe545be6c53d1",
     "url": "/subreddit/static/js/1.84cf428f.chunk.js"
   },
   {
-    "revision": "611a8ab23911ae8cc8a0",
+    "revision": "f06ed6f5a8446e2453ec",
     "url": "/subreddit/static/css/main.74576a3f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/subreddit/static/css/1.455db074.chunk.css"
   },
   {
-    "revision": "671601c07593a8cd79f3dccb2379b659",
+    "revision": "668e056c385dad312457425e1b556d88",
     "url": "/subreddit/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/subreddit/precache-manifest.debd0861bad23948e4d13a837566a66a.js"
+  "/subreddit/precache-manifest.211ad25e47b7b82850e64cba39ab7c11.js"
 );
 
 workbox.clientsClaim();
